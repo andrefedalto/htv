9bcc7d788fd84f381a2dbc16c1aaaf37d9c90090
andrefedalto <andrefedalto@gmail.com>
2015-06-26 23:35:30 +0100

    adding swagger api

---
+++ api/editor/index.php
@@ -0,0 +1,11 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * User: André
+ * Date: 10/05/2015
+ * Time: 16:52
+ */
+
+$path = explode("editor", $_SERVER['PHP_SELF']);
+$url = "http://editor.swagger.io/#/?import=http://" . $_SERVER['HTTP_HOST'].$path[0]."htv.json";
+header("Location: $url");
